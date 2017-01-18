f6667791220c7ade5436ebfc6b623a956ab40be9
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-17 20:32:17 -0800

    updated dynamic card badge icon

--- js/template.js
+++ js/template.js
@@ -158,7 +158,7 @@ TrelloPowerUp.initialize({
       return {
         title: 'Detail Badge', // for detail badges only
         text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-        icon: icon, // for card front badges only
+        icon: './images/logo.png', // for card front badges only
         color: badgeColor,
         refresh: 10
       }

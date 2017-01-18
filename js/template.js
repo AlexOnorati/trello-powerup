6395558ec3f9f5f204d4fb16c858056438d69f11
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-17 20:15:27 -0800

    removed semi collin

--- js/template.js
+++ js/template.js
@@ -156,7 +156,7 @@ TrelloPowerUp.initialize({
   return [
     {
       icon: './images/logo.png',
-      text: t.get('toggle') == null || !t.get('toggle') ? '<span class="icon-sm icon-check checklist-item-checkbox-check"></span>' : '<span class="icon-sm icon-check checklist-item-checkbox-check"></span>';
+      text: t.get('toggle') == null || !t.get('toggle') ? '<span class="icon-sm icon-check checklist-item-checkbox-check"></span>' : '<span class="icon-sm icon-check checklist-item-checkbox-check"></span>'
     }
   ];
 }

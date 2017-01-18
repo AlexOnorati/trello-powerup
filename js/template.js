d8d07e84f7b3d5ec057610dba856714d6c111835
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-17 20:02:47 -0800

    Added card buttons

--- js/template.js
+++ js/template.js
@@ -151,5 +151,17 @@ TrelloPowerUp.initialize({
           });
         }
     }];
-  }
+  },
+  'card-buttons': function(t, options){
+  return [{
+    icon: './images/logo.png',
+    text: 'Button Text',
+    callback: function(t){
+      return t.popup({
+        title: "Card Button Popup",
+        url: './card-button-popup.html'
+      });
+    }
+  }];
+}
 });

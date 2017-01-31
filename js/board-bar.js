b07879e4b86a453eb7c24c29f19b5667e7c7b547
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-30 23:59:39 -0800

    update

--- js/board-bar.js
+++ js/board-bar.js
@@ -11,16 +11,16 @@ t.render(function(){
 
 function GenerateSlides(){
   alert('In generate slides function');
-  var pptx = new PptxGenJS();
-  var slide = pptx.addNewSlide();
-  slide.addText('Hello world!', { x:1.0, y:1.0, font_size:42, color:'00FF00' });
-  pptx.save('Demo-Simple');
+
 }
 
 
 var slideBtn = document.getElementById('create-slides-btn');
 slideBtn.addEventListener('click', function() {
-  alert('Generate Slides!');
-  GenerateSlides();
-  alert('Slides have been generated');
+    alert('Generating slides');
+  var pptx = new PptxGenJS();
+  var slide = pptx.addNewSlide();
+  slide.addText('Hello world!', { x:1.0, y:1.0, font_size:42, color:'00FF00' });
+  pptx.save('Demo-Simple');
+    alert('Generating slides finished');
 });

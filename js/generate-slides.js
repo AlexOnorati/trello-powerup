d590b02d40bf36496f67b4076e0579188aa55c4b
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-31 00:49:01 -0800

    update

--- js/generate-slides.js
+++ js/generate-slides.js
@@ -19,7 +19,6 @@ function GenerateSlides(){
     function(promiseResult){
       for(let i = 0; i < promiseResult.length; i++){
         if(promiseResult[i].idList == selectList.options[selectList.selectedIndex].value){
-          console.log(promiseResult[i]);
           var slide = pptx.addNewSlide();
           slide.addText(promiseResult[i].name, { x:1.0, y:1.0, font_size:42, color:'000000' });
           slide.addText(promiseResult[i].desc, { x:1.0, y:2.0, font_size:24, color:'000000' });
@@ -35,6 +34,5 @@ function GenerateSlides(){
 
 var authBtn = document.getElementById('create-slides-btn');
 authBtn.addEventListener('click', function() {
-  alert('Generate Slides!');
   GenerateSlides();
 });

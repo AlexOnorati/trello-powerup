ada21a26a0e8a3bb1d15b2de63c28083bb8e6f64
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-30 23:58:03 -0800

    update

--- js/board-bar.js
+++ js/board-bar.js
@@ -10,6 +10,7 @@ t.render(function(){
 });
 
 function GenerateSlides(){
+  alert('In generate slides function');
   var pptx = new PptxGenJS();
   var slide = pptx.addNewSlide();
   slide.addText('Hello world!', { x:1.0, y:1.0, font_size:42, color:'00FF00' });

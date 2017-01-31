b16f4ca51c70e0af3d5a9b280211c7424f8b9aa0
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-31 00:01:00 -0800

    update

--- js/generate-slides.js
+++ js/generate-slides.js
@@ -1,14 +1,16 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-var authBtn = document.getElementById('create-slides-btn');
-authBtn.addEventListener('click', function() {
-  alert('Generate Slides!');
-});
-
 function GenerateSlides(){
   var pptx = new PptxGenJS();
   var slide = pptx.addNewSlide();
   slide.addText('Hello world!', { x:1.0, y:1.0, font_size:42, color:'00FF00' });
   pptx.save('Demo-Simple');
 }
+
+
+var authBtn = document.getElementById('create-slides-btn');
+authBtn.addEventListener('click', function() {
+  alert('Generate Slides!');
+  GenerateSlides();
+});

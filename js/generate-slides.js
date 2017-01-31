f836afa9ce8bdd7423e8014fa1dca2ff95cfb9c9
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-31 00:25:18 -0800

    update

--- js/generate-slides.js
+++ js/generate-slides.js
@@ -16,7 +16,7 @@ t.lists('id','name').then(function(promiseResult){
 function GenerateSlides(){
   var pptx = new PptxGenJS();
   var slide = pptx.addNewSlide();
-  slide.addText('Hello world!', { x:1.0, y:1.0, font_size:42, color:'00FF00' });
+  slide.addText(selectList.options[selectList.selectedIndex].innerHTML, { x:1.0, y:1.0, font_size:42, color:'00FF00' });
   pptx.save('Demo-Simple');
 }
 

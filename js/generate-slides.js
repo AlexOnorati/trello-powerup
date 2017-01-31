681d33c4436a4081aa8cfe7c7be70be64c70a246
alexOnorati <aonorati@mail.greenriver.edu>
2017-01-31 00:41:22 -0800

    update

--- js/generate-slides.js
+++ js/generate-slides.js
@@ -18,10 +18,11 @@ function GenerateSlides(){
   t.cards('name', 'desc', 'idList').then(
     function(promiseResult){
       for(let i = 0; i < promiseResult.length; i++){
-        if(promiseResult.idList == selectList.options[selectList.selectedIndex].value){
+        if(promiseResult[i].idList == selectList.options[selectList.selectedIndex].value){
+          console.log(promiseResult[i]);
           var slide = pptx.addNewSlide();
-          slide.addText(promiseResult.name, { x:1.0, y:1.0, font_size:42, color:'000000' });
-          slide.addText(promiseResult.description, { x:1.0, y:2.0, font_size:24, color:'000000' });
+          slide.addText(promiseResult[i].name, { x:1.0, y:1.0, font_size:42, color:'000000' });
+          slide.addText(promiseResult[i].description, { x:1.0, y:2.0, font_size:24, color:'000000' });
         }
       }
         pptx.save(selectList.options[selectList.selectedIndex].innerHTML);

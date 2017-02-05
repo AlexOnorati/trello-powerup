38bb6929bd03fd3eb7e70e33014e51297db73ab0
alexOnorati <aonorati@mail.greenriver.edu>
2017-02-04 23:02:33 -0800

    update

--- js/board-bar.js
+++ js/board-bar.js
@@ -2,25 +2,10 @@
 
 var t = TrelloPowerUp.iframe();
 
-t.render(function(){
-  // this function we be called once on initial load
-  // and then called each time something changes that
-  // you might want to react to, such as new data being
-  // stored with t.set()
-});
-
-function GenerateSlides(){
-  alert('In generate slides function');
-
-}
-
-
 var slideBtn = document.getElementById('create-slides-btn');
 slideBtn.addEventListener('click', function() {
-    alert('Generating slides');
   var pptx = new PptxGenJS();
   var slide = pptx.addNewSlide();
   slide.addText('Hello world!', { x:1.0, y:1.0, font_size:42, color:'00FF00' });
   pptx.save('Demo-Simple');
-    alert('Generating slides finished');
 });

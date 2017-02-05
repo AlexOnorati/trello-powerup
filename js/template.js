4944cd143e2bd3c167645fa728f941b4c39c36a9
alexOnorati <aonorati@mail.greenriver.edu>
2017-02-04 22:58:13 -0800

    update

--- js/template.js
+++ js/template.js
@@ -151,71 +151,44 @@ var cardButtonCallback = function(t){
 };
 
 TrelloPowerUp.initialize({
-
   'board-buttons': function(t, options){
-
     return [{
       icon: WHITE_ICON,
       text: 'Generate',
         callback: function(t){
           return t.popup({
-        title: "Generate Slides",
-        url: './board-button-popup.html'
-      });
-
+            title: "Generate Slides",
+            url: './board-button-popup.html'
+          });
         }
-
     }];
   },
   'card-badges': function(t, card) {
     return t.card('name').then(function(promiseResult){
       var name = promiseResult.name;
       return t.get('card','shared','toggle','T').then(function(promiseGetResult){
-
         return {
-          title: 'Detail Badge', // for detail badges only
+          title: 'Detail Badge',
           text:  promiseGetResult == 'F'?'exclude' : 'include',
-
-          //t.get('toggle') == null || t.get('toggle') ?'exclude' : 'include',
-          icon: './images/logo.png', // for card front badges only
-
+          icon: './images/logo.png',
           refresh: 10
         };
       });
     });
-  // return {
-  //   dynamic: function(){
-  //     var name;
-  //     console.log(t.card('name').then(function(promiseResult){
-  //       return promiseResult.name;
-  //     }));
-  //     return {
-  //       title: 'Detail Badge', // for detail badges only
-  //       text:  'test',
-  //
-  //       //t.get('toggle') == null || t.get('toggle') ?'exclude' : 'include',
-  //       icon: './images/logo.png', // for card front badges only
-  //
-  //       refresh: 10
-  //     }
-  //}
-  //};
-},
-'card-buttons':function(t, options){
-  return t.get('card', 'shared','toggle', 'T').then(function(promiseResult){
-    return [{
-      icon: './images/logo.png',
-      text:  promiseResult == 'F'?'exclude' : 'include',
-      callback: function(t){
-        if(promiseResult == 'T'){
-          t.set('card', 'shared','toggle', 'F');
-        }else{
-          t.set('card', 'shared','toggle', 'T');
+  },
+  'card-buttons':function(t, options){
+    return t.get('card', 'shared','toggle', 'T').then(function(promiseResult){
+      return [{
+        icon: './images/logo.png',
+        text:  promiseResult == 'F'?'exclude' : 'include',
+        callback: function(t){
+          if(promiseResult == 'T'){
+            t.set('card', 'shared','toggle', 'F');
+          }else{
+            t.set('card', 'shared','toggle', 'T');
+          }
         }
-      }
       }];
     });
   }
-
-
 });

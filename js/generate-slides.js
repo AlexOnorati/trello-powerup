c30d1151e93c6b353895d3d22137c1aab3f83223
alexOnorati <aonorati@mail.greenriver.edu>
2017-02-15 02:08:44 -0800

    Update

--- js/generate-slides.js
+++ js/generate-slides.js
@@ -42,28 +42,13 @@ function GenerateSlides(){
 }
 
 function getImage(oldURL) {
-  // Simulate a call to Dropbox or other service that can
-// return an image as an ArrayBuffer.
-var xhr = new XMLHttpRequest();
 
-// Use JSFiddle logo as a sample image to avoid complicating
-// this example with cross-domain issues.
-xhr.open( "GET", oldURL, true );
-
-// Ask for the result as an ArrayBuffer.
-xhr.responseType = "arraybuffer";
-
-xhr.onload = function( e ) {
   // Obtain a blob: URL for the image data.
   var arrayBufferView = new Uint8Array( this.response );
   var blob = new Blob( [ arrayBufferView ], { type: "image/png" } );
-  var urlCreator = window.URL || window.webkitURL;
   var imageUrl = urlCreator.createObjectURL( blob );
   console.log(imageUrl);
   return imageUrl;
-};
-
-xhr.send();
 }
 
 

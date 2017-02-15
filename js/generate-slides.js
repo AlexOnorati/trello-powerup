969bcb594ec8be1a4dbdc2a89af74ec32c552ef8
alexOnorati <aonorati@mail.greenriver.edu>
2017-02-15 02:26:37 -0800

    Update

--- js/generate-slides.js
+++ js/generate-slides.js
@@ -29,7 +29,10 @@ function GenerateSlides(){
           var attachments = promiseResult[i].attachments;
 
           for(let j = 0; j < attachments.length; j++){
-            console.log(getImage(attachments[j].url));
+            console.log(attachments[j].url);
+            toDataUrl('attachments[j].url', function(base64Img) {
+              console.log(base64Img);
+            });
             slide.addImage({x:9.3, y:4.9, w:0.5, h:0.5, data:"image/png;base64,"+getImage(attachments[j].url)});
           }
         }
@@ -50,6 +53,20 @@ function getImage(oldURL) {
   var imageUrl = urlCreator.createObjectURL( blob );
   console.log(blob);
   return imageUrl;
+}
+
+function toDataUrl(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
 }
 
 
